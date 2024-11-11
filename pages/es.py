861fe578b91022a3c15627a28c5b0d4cf9c93b41
Ruhil Hanani <94262083+Ruhilhnni@users.noreply.github.com>
2024-11-12 02:17:08 +0800

    Update es.py

--- pages/es.py
+++ pages/es.py
@@ -14,10 +14,6 @@ import streamlit as st
 st.title("Ackley Function Optimization using Evolution Strategy")
 st.write("This app demonstrates the optimization of the Ackley function using two evolution strategies: (μ, λ) and (μ + λ).")
 
-# Show Ackley Function Plot
-st.subheader("3D Plot of the Ackley Function")
-plot_ackley()
-
 # objective function
 def objective(x, y):
   return -20.0 * exp(-0.2 * sqrt(0.5 * (x**2 + y**2))) - exp(0.5 * (cos(2 * pi * x) + cos(2 * pi * y))) + e + 20
